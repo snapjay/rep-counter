56a0d133a7b96db59fc5870659e20bd8ef3ee86c
Dan Shreim <dan@snapjay.com>
2019-02-20 11:37:39 -0500

    Add icons and manifest

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ class App extends Component {
             <div className="App">
                     <h5>Rep Timer</h5>
                     <StopWatch/>
+                <small>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></small>
             </div>
         )
     }
